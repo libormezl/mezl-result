969059b51b74296cdb416eb96636d36fb95e9b32
Libor Mézl <libor.mezl@eurowag.com>
2024-05-14 09:58:41 +0200

    Extensions

---
+++ Mezl.Result.Extensions/ThenDoExtensionsy.cs
@@ -0,0 +1,49 @@
+﻿namespace Mezl.Result.Extensions
+{
+    public static class ThenExtensions
+    {
+        public static async Task<R<TNextValue>> ThenAsync<TRValue, TNextValue>(this Task<R<TRValue>> resultTask, Func<TRValue, Task<R<TNextValue>>> onValue)
+        {
+            var result = await resultTask;
+            if (result.IsNotSuccessful)
+            {
+                return result.Reason;
+            }
+
+            return await onValue(result.Value)
+                .ConfigureAwait(false);
+        }
+
+        public static async Task<R<TNextValue>> ThenAsync<TRValue, TNextValue>(this R<TRValue> result, Func<TRValue, Task<R<TNextValue>>> onValue)
+        {
+            if (result.IsNotSuccessful)
+            {
+                return result.Reason;
+            }
+
+            return await onValue(result.Value)
+                .ConfigureAwait(false);
+        }
+
+        public static async Task<R<TNextValue>> Then<TRValue, TNextValue>(this Task<R<TRValue>> resultTask, Func<TRValue, R<TNextValue>> onValue)
+        {
+            var result = await resultTask;
+            if (result.IsNotSuccessful)
+            {
+                return result.Reason;
+            }
+
+            return onValue(result.Value);
+        }
+
+        public static R<TNextValue> Then<TRValue, TNextValue>(this R<TRValue> result, Func<TRValue, R<TNextValue>> onValue)
+        {
+            if (result.IsNotSuccessful)
+            {
+                return result.Reason;
+            }
+
+            return onValue(result.Value);
+        }
+    }
+}
